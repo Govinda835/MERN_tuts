9edfa1c49ed368aad4e8afebe7d22727b43218ac
Govinda835 <chaudharygovinda835@gamil.com>
2024-08-13 20:04:59 +0545

    change in res.send

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get("/",(req,res)=>{
-    res.send("hello movies.djsa,hdsgkdahsjlhsadjshdkjhsd...")
+    res.send("hello movies.")
     
 })
 app.get("/home",(req,res)=>{
--- nothing.js
+++ nothing.js
@@ -23,19 +23,35 @@
 // app.listen(450,()=>
 // {
 //     console.log("is this server working or not?")
+// // })
+
+// const express = require('express')
+// const app = express()
+// app.get("/",(req,res)=>{
+//     res.send("Hello World, my name is govinda chaudhary.")
 // })
+// app.get()
+// app.listen(4500,()=>
+// {
+//     console.log("my name is govinda chaudhary.")
+// })
+
 
 const express = require('express')
 const app = express()
-app.get("/",(req,res)=>{
-    res.send("Hello World, my name is govinda chaudhary.")
-})
-app.get()
-app.listen(4500,()=>
+app.get("/",(req,res)=>
 {
-    console.log("my name is govinda chaudhary.")
+    res.json({
+        message:"hello mother fuckers."})
 })
-
-
+app.get("/home",(req,res)=>
+{
+    res.json({
+        message: "This is home page of the sever 3456."
+    })
+})
+app.listen(3456,()=>{
+    console.log("is our 3456 server working or not?")
+})
 
 
