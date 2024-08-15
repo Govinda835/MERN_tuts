145c6b0bf74070d25c80947a1aeb6052364b5346
Govinda835 <chaudharygovinda835@gamil.com>
2024-08-15 21:23:47 +0545

    model connected and data is sent successfully

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 require("dotenv").config();
 const express = require("express");
-// const connectdb = require('./database/index.js')
+
 const connectToDatabase = require("./database");
+const { Blog } = require("./model/blog.model");
 
 const app = express();
-// connectdb()
+app.use(express.json())
 connectToDatabase();
 app.get("/", (req, res) => {
-  res.json({
+  res.status(200).json({
     message: "hello movies.",
   });
 });
 
 app.get("/home", (req, res) => {
-  res.json({
+  res.status(200).json({
     message: "this is a home page.",
   });
 });
+
+app.post("/blog",async(req,res)=>{
+    console.log(req.body)
+    const {title, subtitle, description, image}= req.body
+    const response = await Blog.create({
+        title,
+        subtitle,
+        description,
+        image
+    })
+    res.status(200).json({
+        message : "blog API hit successfully..!!"
+    })
+})
+
 
 app.listen(process.env.PORT, () => {
   console.log("server is listening at 3000 port.....!!!!");
